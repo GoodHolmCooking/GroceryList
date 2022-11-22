4955e11298153faf672cbedb14c38bed68cd0755
Ryan Holm <rholm667@gmail.com>
2022-11-22 16:09:16 -0500

    init commit

---
+++ GroceryList/Controllers/GroceryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GroceryList.Models;
+
+namespace GroceryList.Controllers
+{
+    public class GroceryController : Controller
+    {
+        private static List<GroceryItem> groceryItems = new List<GroceryItem>();
+        public IActionResult Index()
+        {
+            return View(groceryItems);
+        }
+
+        public IActionResult Add()
+        {
+            var item = new GroceryItem();
+            return View(item);
+        }
+
+        public IActionResult SaveItem(GroceryItem groceryItem)
+        {
+            groceryItems.Add(groceryItem);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ GroceryList/Models/GroceryItem.cs
@@ -0,0 +1,11 @@
+﻿namespace GroceryList.Models
+{
+    public class GroceryItem
+    {
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Category { get; set; }
+    }
+}
